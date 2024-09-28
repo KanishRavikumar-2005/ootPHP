d3b4a6507ed8edcd88c7f00a101bbfb10f409c3e
Kanish R <85105070+KanishRavikumar-2005@users.noreply.github.com>
2024-09-28 15:51:37 +0530

    Add files via upload

---
+++ config/_cdns.php
@@ -0,0 +1,6 @@
+<?php 
+$CDNS = [
+ "jquery" => ["script", "https://code.jquery.com/jquery-3.6.4.min.js"],
+  "iconscout" => ["style", "https://unicons.iconscout.com/release/v4.0.8/css/line.css"]
+]
+?>
---
+++ config/_init.php
@@ -0,0 +1,8 @@
+<?php 
+$rootPath = $_SERVER['DOCUMENT_ROOT'];
+
+require_once($rootPath . '/routing/Router.php');
+require_once($rootPath . '/routing/Renderer.php');
+require_once($rootPath . '/.config/_ajax.cont.php');
+
+?>
